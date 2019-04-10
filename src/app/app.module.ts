69929c47f61809a30ba27c3a27ef87009d23a00e
Pulkit Singhal <pulkitsinghal@gmail.com>
2019-04-09 22:35:22 -0700

    Added common material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModule } from 'src/app/material.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule
+    BrowserModule
+    ,BrowserAnimationsModule
+    ,MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule
+  ,MatCardModule
+  ,MatCheckboxModule
+  ,MatFormFieldModule
+  ,MatGridListModule
+  ,MatIconModule
+  ,MatInputModule
+  ,MatListModule
+  ,MatMenuModule
+  ,MatRadioModule
+  ,MatSidenavModule
+  ,MatTableModule
+  ,MatToolbarModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule
+    ,MatCardModule
+    ,MatCheckboxModule
+    ,MatFormFieldModule
+    ,MatGridListModule
+    ,MatIconModule
+    ,MatInputModule
+    ,MatListModule
+    ,MatMenuModule
+    ,MatRadioModule
+    ,MatSidenavModule
+    ,MatTableModule
+    ,MatToolbarModule
+  ],
+  exports: [
+    MatButtonModule
+    ,MatCardModule
+    ,MatCheckboxModule
+    ,MatFormFieldModule
+    ,MatGridListModule
+    ,MatIconModule
+    ,MatInputModule
+    ,MatListModule
+    ,MatMenuModule
+    ,MatRadioModule
+    ,MatSidenavModule
+    ,MatTableModule
+    ,MatToolbarModule
+  ]
+})
+export class MaterialModule {}
